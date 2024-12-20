4eb95d0a4f9903deada094ad87ec74839c82075a
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-12-20 22:14:37 +0500

    added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import payment from '../../assets/Images/footer end payment.png'
 
 const Footer = () => {
   return (
+    <main className='bg-[#FFF8F6]'>
     <section className='bg-[#FFF8F6] p-10 mt-10 grid md:grid-cols-3 gap-20 grid-cols-1'>
         <div className='flex flex-col items-center gap-2'>
             <h1 className='fontroman text-[20px] font-medium'>GOOD4ME.</h1>
@@ -12,7 +14,7 @@ const Footer = () => {
             <p className='text-center text-[#5e5f66] font-light text-[16px]'>Subscribe to receive new product updates, be the first to know about sales, and more.</p>
             <form action="" className='md:w-80 flex outline-none border-b-black border-t-transparent border-x-transparent  border-2'>
                 <input type="email" placeholder='Enter Your Email' className='md:w-80 font-medium text-[16px] bg-transparent text-[#9FA8B3] outline-none '/>
-                <button className='text-[#F7623E] font-medium hover:scale-[1.15] transition duration-150'>Subscribe</button>
+                <button className='text-[#F7623E] font-medium hover:scale-[1.15] transition duration-150 uppercase'>Subscribe</button>
                 </form>
         </div>
         <div className='flex flex-col Jost-reg gap-2 text-[20px] items-center text-center md:text-start'>
@@ -29,6 +31,21 @@ const Footer = () => {
             </div>
       
     </section>
+    <section className='Jost-reg flex flex-col items-center gap-4 md:grid md:grid-cols-3 md:gap-20 grid-cols-1 pt-3 px-2 border-t-[#e8e6e8] border-[1px] text-center'>
+      <div><ul className='flex gap-3 order-1'>
+        <li><a href=""><i class="fa-brands fa-square-facebook text-[#6C757D] text-[24px]"></i></a></li>
+        <li><a href=""><i class="fa-brands fa-instagram text-[#6C757D] text-[24px]"></i></a></li>
+        <li><a href=""><i class="fa-brands fa-twitter text-[#6C757D] text-[24px]"></i></a></li>
+        <li><a href=""><i class="fa-brands fa-linkedin text-[#6C757D] text-[24px]"></i></a></li>
+        <li><a href=""><i class="fa-brands fa-tiktok text-[#6C757D] text-[24px]"></i></a></li>
+      </ul></div>
+      <div className='text-[16px] text-center order-3 md:order-2'>
+        <p >© 2024, <span className='text-[#F7623E]'>GOOD4ME.</span> Powered by</p>
+        <p>Javeriya and React</p>
+      </div>
+      <div className='md:order-3 '><img src={payment} alt="images" className='' /></div>
+    </section>
+    </main>
   )
 }
 
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,8 +5,8 @@ import './hero.css'
 const Hero = () => {
   return (
     <>
-      <div className='w-100 relative'>
-        <div className='z-10 absolute'>
+      <div className='w-100 relative hero-bg pb-10'>
+        <div className='z-10 absolute hidden sm:block'>
         <img src={editedHero} />
         </div>
         <div className='grid grid-col-1 sm:grid-cols-2'>
@@ -15,9 +15,9 @@ const Hero = () => {
           {/* 2nd col  */}
         <div className='z-20 flex flex-col items-center pt-10 gap-3'>
         <h1 className='text-[28px] Jost-reg text-[#F6623E] font-medium text-center'>Good4Me</h1>
-        <h1 className='Jost-reg text-[40px] font-medium text-center'>Apple Cider Vinegar</h1>
-        <div className='text-center md:w-8/12'><p className='text-[18px] Jost-reg font-normal text-[#21252E]'>Good4Me Apple Cider Vinegar gummies are the newest addition to your morning health and well-being regime.</p></div>
-        <button className='bg-[#F6623E] text-white rounded-lg py-2 px-3 Jost-reg'>SHOP NOW</button>
+        <h1 className='Jost-reg text-[30px] sm:text-[40px] font-medium text-center'>Apple Cider Vinegar</h1>
+        <div className='text-center md:w-8/12'><p className='text-[14px] sm:text-[18px] Jost-reg font-normal text-[#21252E]'>Good4Me Apple Cider Vinegar gummies are the newest addition to your morning health and well-being regime.</p></div>
+        <button className='bg-[#F6623E] text-white rounded-lg py-2 px-3 Jost-reg text-[12px] sm:text-[16px]'>SHOP NOW</button>
         </div>
         </div>
 
